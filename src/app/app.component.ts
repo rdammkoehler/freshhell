d012f628cb587b643656326740dd92b4dfbf9ee7
Rich Dammkoehler <rpd@noradltd.com>
2019-06-24 15:35:36 -0500

    working toward better stored data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,10 +9,17 @@ import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 export class AppComponent implements OnInit {
 
   formGroup: FormGroup;
-  hells: string[] = [];
+  hells: object[] = [];
+  stats = {
+    // count: 0,
+    // histogram: []
+  }; // probably not very type-scripty
+  optIn = false;
 
   constructor(private formBuilder: FormBuilder) {
     this.hells = localStorage.getItem('hells') ? JSON.parse(localStorage.getItem('hells')) : [];
+    this.stats = localStorage.getItem('stats') ? JSON.parse(localStorage.getItem('stats')) : {count: 0};
+    this.optIn = localStorage.getItem('optIn') ? JSON.parse(localStorage.getItem('optIn')) : false;
   }
 
   ngOnInit(): void {
@@ -23,8 +30,38 @@ export class AppComponent implements OnInit {
     if (this.formGroup.invalid) {
       return;
     }
-    this.hells = [`${this.formGroup.value.hell} was observed at ${new Date()}`, ...this.hells];
+    const freshHell = {
+      description: this.formGroup.value.hell,
+      observed: new Date()
+    };
+    this.hells = [
+      freshHell,
+      ...this.hells
+    ];
     this.formGroup.reset();
-    localStorage.setItem('hells', JSON.stringify(this.hells.slice(0, 1000)));
+    localStorage.setItem('hells', JSON.stringify(this.hells.slice(0, 9)));
+    // if (this.stats.histogram) {
+    //   const key = freshHell.observed.toLocaleDateString().split('').reverse().join('').replace(/\//g, '');
+    //   if (this.stats.histogram[key]) {
+    //     this.stats.histogram[key] = this.stats.histogram[key] + 1;
+    //   } else {
+    //     this.stats.histogram[key] = 1;
+    //   }
+    // }
+    /* So what do you want to track?
+      - Total Hells
+      - Hells / Timeperiod
+      - Average time between observations
+      - what else?
+      - User settings (but not necessarily here)
+        - opt-in for reporting to the cloud
+        - ???
+     */
+    this.stats.count = this.stats.count + 1,
+      localStorage.setItem('stats', JSON.stringify(this.stats));
+    // IF opt-in, then we should send the fresh hell to a service!
+    if (this.optIn) {
+      // make an call to the endpoint
+    }
   }
 }
